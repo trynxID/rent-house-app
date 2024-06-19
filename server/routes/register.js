fc7518660d99e25801514a37fc687585823ea37d
DedeWahyu <dedeanugrah12@gmail.com>
2024-06-19 22:24:19 +0700

    Hapus folder client dan server

--- server/routes/register.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const registerController = require("../controllers").register;
-
-router.post("/save", registerController.addUser);
-
-module.exports = router;
