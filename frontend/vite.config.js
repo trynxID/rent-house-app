81802b0a77b79d4ad18a7fe4949d170d570ca078
trainerBE3 <163844685+trainerBE3@users.noreply.github.com>
2024-06-20 12:14:43 +0700

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,15 +5,16 @@ import react from '@vitejs/plugin-react-swc'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 5000,
+    port: 4573,
+    // port: 5000,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:4573',
         changeOrigin: true,
         secure: false,
       },
       '/public': {
-        target: 'http://localhost:3000',
+        target: 'http://localhost:4573',
         changeOrigin: true,
       },
     },
